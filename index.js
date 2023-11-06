862db6eab68fce54890dffbf48a9d566a9aece1b
Chirag Shriyan <shubhamshiryan123@gmail.com>
2023-11-06 21:31:37 +0530

    get all user and find by id on user route  done

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use('/api/login', require('./routes/loginRoute'));
 app.use('/api/signup', require('./routes/signupRoute'));
 app.use('/api/product', require('./routes/productRoute'));
 app.use('/api/user', require('./routes/userRoute'));
-app.use('/api/admin', require('./routes/adminRoute'));
 
 app.listen(PORT, async () => {
     await dbConnect();
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,23 +1,65 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 const UserModel = require('../models/UserModel');
 const AdminModel = require('../models/AdminModel');
+const { isValidObjectId } = require('mongoose');
+
+function hasRole(roleArr, findRole) {
+
+    for (let i = 0; i < roleArr.length; i++) {
+        const role = roleArr[i];
+        if (role === findRole) {
+            return true;
+        }
+    }
+    return false;
+}
 
 router.get('/', async (req, res) => {
 
     try {
-        const userId = req.headers.userId;
+        const adminId = req.headers.admin_id;
+        const isValidAdminId = isValidObjectId(adminId);
 
-        const isAdmin = AdminModel.findById({ _id: id });
-        const limit = 5;
+        if (isValidAdminId) {
+            const admin = await AdminModel.findOne({ userId: adminId }).select('role');
+            const limit = 5;
+            const isAdmin = hasRole(admin.role, 'superAdmin');
 
-        if (isAdmin) {
-            const user = await UserModel.find({}).limit(limit);
+            if (isAdmin) {
+                const user = await UserModel.find({}).limit(limit);
+                res.status(200).send({ data: user });
+            }
+            else {
+                res.status(200).send({ message: `Access Denied` });
+            }
+
+        }
+        else {
+            res.status(400).send({ message: `The ObjectId:${adminId} is invalid` });
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        const isValidId = isValidObjectId(id);
+
+        if (isValidId) {
+            const user = await UserModel.findById(id).select('email username');
             res.status(200).send({ data: user });
         }
+        else {
+            res.status(400).send({ message: `The ObjectId:${id} is invalid` });
+        }
+
+
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
